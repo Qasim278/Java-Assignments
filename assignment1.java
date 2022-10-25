fcf8ba8df4cf430d484155d54e304b2141162f7a
Qasim278 <78903769+Qasim278@users.noreply.github.com>
2022-10-25 15:01:36 +0300

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,74 @@
+package programming1;
+
+import java.util.Scanner;
+
+public class assignment1 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter a string: ");
+
+		String input = scan.nextLine();
+
+		boolean palindrome = isPalindrome(input);
+
+		if (palindrome) {
+
+			System.out.println("The text is a palindrome.");
+
+		} else {
+
+			System.out.println("No palindrome");
+		}
+
+	}
+
+	public static boolean isPalindrome(String input) {
+
+		String allowed = "[A-ZÅÄÖa-zåäö\\ \\.\\,\\:\\;\\?\\!\\-\\'\\\"]{0,}";
+		String noLetters = "[\\ \\.\\,\\:\\;\\?\\!\\\\-\\\\'\\\"]{0,}";
+
+		if (input.matches(noLetters)) {
+			return false;
+		} else if (input.matches(allowed)) {
+
+			String notTrim = "[\\ \\.\\,\\:\\;\\?\\!\\-\\'\\\"]{1,}]";
+			boolean trimDone = false;
+			String trimmedStr = "";
+
+			while (!trimDone) {
+
+				String trimmed = input.replaceAll("[\\ \\.\\,\\:\\;\\?\\!\\-\\'\\\"]", "");
+
+				if (trimmed.matches(notTrim)) {
+
+					trimDone = false;
+				} else {
+
+					trimDone = true;
+					trimmedStr = trimmed.toLowerCase();
+
+				}
+
+			}
+
+			String reversedStr = new StringBuilder(trimmedStr).reverse().toString().toLowerCase();
+
+
+			if (trimmedStr.equals(reversedStr)) {
+
+				return true;
+			} else {
+
+				return false;
+			}
+
+		} else {
+			return false;
+		}
+
+	}
+
+}
